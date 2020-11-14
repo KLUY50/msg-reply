202f5acd7d90fa1bf3db8330d7814c3da8cd9349
Darkavix <dasshane305@gmail.comm>
2020-11-14 10:03:22 +0700

    update function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const http = require("http")
+
 function reply(msg) {
-    return msg;
+    http.get(`https://console.dialogflow.com/api-client/demo/embedded/e0249676-22cf-4cd0-ab44-75c0b9d7ecff/demoQuery?q=${encodeURI(msg)}&sessionId=7990e532-3e33-6c24-a9b5-042fb5a9c1d7`, (res) => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const data = JSON.parse(rawData);
+                return data.result.fulfillment.speech;
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
+    
 }
 
 module.exports.reply = reply;
